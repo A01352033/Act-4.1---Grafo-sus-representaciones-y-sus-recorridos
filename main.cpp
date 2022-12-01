3595536008935bbe443a22548387644256468881
A01352033 <110910816+A01352033@users.noreply.github.com>
2022-12-01 15:55:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include "matriz.cpp"
+#include "lista.cpp"
 
+int main(int argc, char *argv[])
+{
+    listagrafica<char> graph(true);
+    graph.vertice('A');
+    graph.vertice('B');
+    graph.vertice('C');
+    graph.vertice('D');
+    graph.vertice('E');
+    graph.punto('A', 'B');
+    graph.punto('A', 'C');
+    graph.punto('B', 'C');
+    graph.punto('B', 'D');
+    graph.punto('C', 'D');
+    graph.punto('C', 'E');
+    graph.punto('D', 'E');
+    graph.dfs('B');
+    std::cout << std::endl;
+    graph.bfs('B');
+    std::cout << std::endl;
+    std::cout << graph;
+    return 0;
+}
